7b399bb0865afd2afd3af30d413118900f3aa275
Oberfeldwedler <samuelbrunner8@gmail.com>
2025-10-11 17:17:45 +0200

    f

---
+++ classes/mysql.py
@@ -0,0 +1,94 @@
+import pymysql.cursors
+
+from lib import classes
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class MySql:
+    def __init__(self, mySqlSettingsDict):
+        self.__connectionState = "never"
+        self.hostAddress = mySqlSettingsDict["host_address"]
+        self.portNumber = int(mySqlSettingsDict["port_number"])
+        self.username = mySqlSettingsDict["username"]
+        self.password = mySqlSettingsDict["password"]
+        self.database = mySqlSettingsDict["database"]
+        self.connection = None  
+
+    def getConnectionState(self):
+        return self.__connectionState
+
+    def establishConnection(self):
+        try:
+            logger.debug("Trying to connect to database:")
+            logger.debug(f"HOST={self.hostAddress}:{self.portNumber}, USER={self.username}, DB={self.database}")
+            self.connection = pymysql.connect(user=self.username, password=self.password,
+                                    host=self.hostAddress, port=self.portNumber,
+                                    database=self.database, 
+                                    read_timeout=1, write_timeout=1, connect_timeout=1)
+            self.dictCursor = self.connection.cursor(pymysql.cursors.DictCursor)
+            self.__connectionState = "up"
+            logger.info("Connection to database established.")
+            return True
+        except Exception as err:
+            logger.error("Cannot connect to database!")
+            logger.error(err)
+            self.__connectionState= "down"
+            return False
+        
+    def closeConnection(self):
+        logger.info("Connection to database closed.")
+        try:
+            self.connection.close()
+        except:
+            logging.warning("Connection to database could not be closed! Ignoring!")
+            pass
+
+    """
+    Checks whether there is a working database connection 
+    and reestablishes when there isn't.
+
+    Args: 
+        None
+    Returns:
+        connectionState: The connection state to the MySql server
+        connectionStateChanged: This indicates, whether the connection
+            state is now different to the one specified in 
+            __connectionState.
+            This information is used by the GUI to update the layout.
+    """
+    def ensureDatabaseConnection(self):
+        lastConnectionState = self.__connectionState
+        if self.connection == None:
+            # Here we land only when the application has just been started and a database connection has not yet been established.
+            # This is the regular way for connection to be established for the first time.
+            self.establishConnection()
+        else:
+            try:
+                self.connection.ping(reconnect=True)
+                self.__connectionState = "up"
+            except:
+                self.__connectionState = "down"
+
+        if not self.__connectionState == lastConnectionState:
+            connectionStateChanged = True
+        else:
+            connectionStateChanged = False
+
+        return self.__connectionState, connectionStateChanged
+
+    def insertDataset(self, dataset):
+        insertPurchase = (
+            "INSERT INTO udp_data (product_id, user_id, price_then, price_factor_then) VALUES (%s, %s, %s, %s)"
+        )
+        try:
+            logger.debug(self.dictCursor.mogrify(insertPurchase, ( product.id, user.id, product.price, user.price_factor) ))
+            self.dictCursor.execute(insertPurchase, ( product.id, user.id, product.price, user.price_factor) )
+            return True
+        except Exception as err:
+            self.connection.rollback()
+            logger.error("Failed to insert purchase into database:")
+            logger.error(err)
+            return False
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import sys
 
+from classes import mysql
 
 try:
     import tomllib
@@ -10,12 +12,68 @@ except ImportError:
 
 from classes import serial_transmitter
 from classes import udp_receiver
-from classes import data_tools
 
 
 with open("config.toml", "rb") as f:
     config = tomllib.load(f)
 
 general_settings_dict = config['general']
+mysql_settings_dict = config['mysql']
+udp_settings_dict = config['udp']
 
+
+log_location = general_settings_dict.get("log_location", "./log")
+if not os.path.exists(log_location):
+    os.mkdir(log_location)
+
+logger = logging.getLogger()
+logger.setLevel(general_settings_dict.get("log_level", "INFO").upper())
+
+formatter = logging.Formatter(
+    "[%(levelname)-7s] [%(asctime)s] %(name)10s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+file_handler = logging.handlers.RotatingFileHandler(
+    f"{log_location}/udp_to_serial.log", maxBytes=(1048576*5), backupCount=7, encoding='utf-8'
+)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+# Stream handler (stdout -> captured by systemd)
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+file_handler.doRollover()
+
+# Catch unhandled exceptions in main thread
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        # Let KeyboardInterrupt go through without logging
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+    logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+sys.excepthook = handle_exception
+
+logger.warning("=========== NEW START OF udp-to-serial-converter ===========")
+
+receiver = udp_receiver.UdpReceiver(udp_settings_dict)
+database_caller = mysql.MySql(mysql_settings_dict)
+
+
+try: 
+    while 1:
+        dataset = receiver.read()
+
+        #TODO: parse data here
+
+
+        database_caller.insertDataset(dataset)
+
+finally:
+    receiver.close()
+    # transmitter.close()
+    
 
